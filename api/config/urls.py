1b91e938b2cf5c746d6a7f3b060744cc11cc93d5
AdrianHP <adrianmatcom@gmail.com>
2024-03-02 17:41:54 -0500

    refactoring code

--- api/config/urls.py
+++ api/config/urls.py
@@ -18,12 +18,7 @@ from django.urls import path, include
 from task_manager import views
 
 urlpatterns = [
-    path('users/', views.list_users),
-    path('users/<int:user_id>/tasks/', views.list_users_tasks),
-    path('tasks/<int:task_id>/', views.get_task_details),
-    path('task/add/', views.add_task),
-    path('task/edit/', views.edit_task),
-    path('task/delete/', views.delete_task),
-    path('task/getall/', views.get_tasks),
-     path('login/', include('task_manager.login.urls'))
+   
+     path('login/', include('task_manager.login.urls')),
+     path('task/', include('task_manager.tasks.urls'))
 ]
